6319425e39f3e0a7b33b8b5febfdb9333c4a55a3
Pratyush-Remon <pratyushsoni2004@gmail.com>
2023-04-15 03:00:50 +0530

    added bot verification function

--- bin/func.py
+++ bin/func.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-
+from time import sleep
 options = Options()
 options.add_experimental_option("detach", True)
 
@@ -25,6 +25,14 @@ def remove_duplicates(links):
             unique_links.append(link)
     return unique_links
 
+def click_table_button(addr, button_num):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get(addr)
+    button_elem = driver.find_element(By.CSS_SELECTOR, f"a[href='#{button_num}']")
+    button_elem.click()
+    sleep(4)  # Wait for table to update (adjust time as necessary)
+    click_im_human_button(driver)
+
 def save_links_to_file(links):
     import os
     folder_name = "my_links"
@@ -36,11 +44,20 @@ def save_links_to_file(links):
         file_name = f"my_links/links({i}).txt"
         i += 1
     with open(file_name, "w") as f:
-        f.write("\n".join(links))
+        for link in links:
+            parts = link.split("/")
+            cleaned_link = "/".join([parts[0], "", "", parts[6]])
+            f.write(cleaned_link + "\n")
     return file_name
+def click_im_human_button(driver):
+    im_human_button = driver.find_element(By.ID, "captcha_submit")
+    im_human_button.click()
+
 addr = "https://myip.ms/browse/sites/1/ipID/23.227.38.0/ipIDii/23.227.38.255/sort/2/asc/1/#2"
 
 links = get_links(addr)
 unique_links = remove_duplicates(links)
 file_name = save_links_to_file(unique_links)
 print(f"Links saved to file: {file_name}")
+click_table_button(addr,2)
+click_im_human_button()
